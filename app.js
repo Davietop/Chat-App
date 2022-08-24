ca378463dca55dab7b6996412d1eacadca9ae301
Tayo-Subair Fuad <tayosubairfuad@gmail.com>
2022-08-24 02:28:39 +0100

    grid 1 to 4

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ document.querySelector(".inbox").addEventListener('click', ()=> {
     document.querySelector(".inbox").classList.toggle("list-toggle-1")
 })
 
+let msg= document.getElementById("msgtype")
+
+msg.addEventListener("click", ()=> {
+    alert("Hi")
+})
